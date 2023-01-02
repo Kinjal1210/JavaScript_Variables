749f1d7fa30ff5a3b0b10ae1024bb464d92906cc
kinjal patel <kinjalpatel@kinjals-MacBook-Air.local>
2023-01-02 15:24:07 -0500

    app.js git commit -m app.js

--- app.js
+++ app.js
@@ -3,7 +3,33 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Kunjal";
+let lastName = "Patel";
+var age = 38;
+
+lastName ="kinjal";
+age = 28;
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+
+
+
 
 // Exercise 2
 
